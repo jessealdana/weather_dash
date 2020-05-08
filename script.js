68094971c5c88c869dd76dd672c3fe52d2cc2af9
Jesse Aldana <jesse.aldana@gmail.com>
2020-05-08 16:37:21 -0700

    restored current weather data and added beginning CSS

--- script.js
+++ script.js
@@ -1,61 +1,63 @@
- console.log("here")         
-$(document).ready(function() {
- 
-    $("#find-city").on("click", function(event) {
+//  console.log("here")         
+// $(document).ready(function() {
+//  console.log("hello")
+//     $("#find-city").on("click", function(event) {
+//         console.log("hola")
+//         //text from the input box
+//         var city = $("#city-input").val();
 
-        //text from the input box
-        var city = $("#city-input").val();
+//         if (city === "") {
+          console.log("bonjour")
+          //query the database for current weather of userChoice city
+          var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + "Los Angeles&units=imperial&appid=eab78c727c5c9a0c1ae2d2b96188e8ea";
 
-        if (city === "") {
-        //query the database for current weather of userChoice city
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=eab78c727c5c9a0c1ae2d2b96188e8ea";
-
-        //AJAX call to the OpenWeatherMap API
-        $.ajax({
+          //AJAX call to the OpenWeatherMap API
+          $.ajax({
                 url: queryURL,
                 method: "GET"
-        })
+          })
           //store all of the retrieved data inside of an object called "response"
-        .then(function(response) {
-            console.log(queryURL);
-            console.log(response);
+          .then(function(response) {
+              console.log(queryURL);
+              console.log(response);
 
           // Transfer content to HTML
             $(".city").html("<h1>" + response.name + " Weather Details</h1>");
             $(".wind").text("Wind Speed: " + response.wind.speed);
             $(".humidity").text("Humidity: " + response.main.humidity);
-            $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-        });
+            $(".temp").text("Temperature (F) " + response.main.temp);
+          });
 
-                const date = new Date();
-                console.log(date)
-                const hour = date.getHours();
-                const mod = Math.floor((24-hour)/3);
-                event.preventDefault();
+//                 //create mod to account for the new forecast updated by the app every 3 hours, or 8 times a day
+//                 const date = new Date();
+//                 console.log(date)
+//                 const hour = date.getHours();
+//                 const mod = Math.floor((24-hour)/3);
+//                 event.preventDefault();
 
-                //query the database for forecast
-                var queryURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&appid=eab78c727c5c9a0c1ae2d2b96188e8ea";
+//                 //query the database for forecast
+//                 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + city + "&appid=eab78c727c5c9a0c1ae2d2b96188e8ea";
            
-                //AJAX call to the OpenWeatherMap API
-                $.ajax({
-                url: queryURL,
-                method: "GET"
-                })
-                //store all of the retrieved data inside of an object called "response"
-                .then(function(response) {
-                    for(let i = 0 ;i<40; i+=8){
-                        $("#forecast").append(`<div class="card col-2" style='margin:auto'>
-                                                <div class="card-body">
-                                                <h5 class="card-title">${response.city.name} <img src="https://openweathermap.org/img/wn/${response.list[i+mod].weather[0].icon}.png"/></h5>
-                                                <p class="card-text">${response.list[i+mod].weather[0].description}</p>
-                                                <p class="card-text">Temperature - ${response.list[i+mod].main.temp}</p>
-                                                <p class="card-text">Wind Speed - ${response.list[i+mod].wind.speed}</p>
-                                                <p class="card-text">Humidity - ${response.list[i+mod].main.humidity}</p>
-                                                </div>
-                                                </div>`)
-                                                }
+//                 //AJAX call to the OpenWeatherMap API
+//                 $.ajax({
+//                 url: queryURL,
+//                 method: "GET"
+//                 })
+//                 //store all of the retrieved data inside of an object called "response"
+//                 .then(function(response) {
+//                     for(let i = 0 ;i<40; i+=8){
+//                         $("#forecast").append(`<div class="card col-2" style='margin:auto'>
+//                                                 <div class="card-body">
+//                                                 <h5 class="card-title">${response.city.name} <img src="https://openweathermap.org/img/wn/${response.list[i+mod].weather[0].icon}.png"/></h5>
+//                                                 <p class="card-text">${response.list[i+mod].weather[0].description}</p>
+//                                                 <p class="card-text">Temperature - ${response.list[i+mod].main.temp}</p>
+//                                                 <p class="card-text">Wind Speed - ${response.list[i+mod].wind.speed}</p>
+//                                                 <p class="card-text">Humidity - ${response.list[i+mod].main.humidity}</p>
+//                                                 </div>
+//                                                 </div>`)
+//                                                 }
 
-                });
-              }        
-    }
-})
+//                 });
+        // }        
+    // })
+// })
